0e3b08573ad3248dafbe4824e4693644c6f5f08f
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-05-07 13:18:04 -0400

    Pass tests

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const scubaDrivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(scubaDrivers){
+    const driver = scubaDrivers.slice(0,2);
+    return driver;
+}
+
+const returnLastTwoDrivers = function(scubaDrivers){
+    return scubaDrivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier){
+    return function(num){
+        return num * multiplier;
+    }
+}
+
+function fareDoubler(fare){
+    return createFareMultiplier(2)(fare)
+}
+
+function fareTripler(fare){
+    return createFareMultiplier(3)(fare)
+}
+
+function selectDifferentDrivers(scubaDrivers, callback){
+    return callback(scubaDrivers);
+}
